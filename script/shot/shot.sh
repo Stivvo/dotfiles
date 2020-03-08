d1be86d642d3861eebba73fb11f02c8086b1c824
Stivvo <entattis15@itisvinci.com>
2020-03-08 11:13:24 +0100

    fancier screenshot notifications
    - icon of the screenshot in the notification
    - when clicking on the notification, ranger will open in the screen
      folder with the screenshot selected
    - after taking the screenshot, dmenu will prompt the user to save the
      screenshot in the default folder (~/screen), or choose it with ranger

--- script/shot/shot.sh
+++ script/shot/shot.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
-NAME=~/screen/scrn_$(date +'%Y-%m-%d_%H-%M-%S').png
+scrnName="scrn_$(date +'%Y-%m-%d_%H-%M-%S').png"
+select="$(echo -e "default\nchoose" | dmenu -i -p "${scrnName} folder")"
+
+if [ $select == "default" ]
+then
+    scrnPath="/home/stefano/screen/${scrnName}"
+else
+    urxvt -e ranger --show-only-dirs --choosedir=/tmp/scrnDir.txt
+    scrnPath="$(cat /tmp/scrnDir.txt)/${scrnName}"
+fi
+
 case "$1" in
     "all")
-        grim $NAME
+        grim $scrnPath
         ;;
     "area")
-        grim -g "$(slurp)" $NAME
+        grim -g "$(slurp)" $scrnPath
         ;;
     "focused")
-        grim -o $(swaymsg -t get_outputs | jq -r '.[] | select(.focused) | .name') $NAME
+        grim -o $(swaymsg -t get_outputs | jq -r '.[] | select(.focused) | .name') $scrnPath
         ;;
 esac
 
-
+notify-send.sh $scrnPath -i $scrnPath --default-action="urxvt -e ranger --selectfile=${scrnPath}"
+#selectfile is a legacy option
