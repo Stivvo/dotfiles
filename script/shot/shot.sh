68ab40c64e66b2b0877cec2f5795b6a6f6224fd0
Stivvo <entattis15@itisvinci.com>
2020-03-09 11:30:54 +0100

    make screenshot before
    when print and variants are pressed, the screenshot is taken immediately,
    and saved in /tmp. Then the user is prompted to selecte the place where
    he wants to move it. This eliminates the delay between the button press
    and the actual screenshot.

--- script/shot/shot.sh
+++ script/shot/shot.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
 scrnName="scrn_$(date +'%Y-%m-%d_%H-%M-%S').png"
+tmpPath="/tmp/${scrnName}"
+
+case "$1" in
+    "all")
+        grim $tmpPath
+        ;;
+    "area")
+        grim -g "$(slurp)" $tmpPath
+        ;;
+    "focused")
+        grim -o $(swaymsg -t get_outputs | jq -r '.[] | select(.focused) | .name') $tmpPath
+        ;;
+esac
+
 select="$(echo -e "default\nchoose" | dmenu -i -p "${scrnName} folder")"
 
 if [ $select == "default" ]
@@ -11,17 +25,7 @@ else
     scrnPath="$(cat /tmp/scrnDir.txt)/${scrnName}"
 fi
 
-case "$1" in
-    "all")
-        grim $scrnPath
-        ;;
-    "area")
-        grim -g "$(slurp)" $scrnPath
-        ;;
-    "focused")
-        grim -o $(swaymsg -t get_outputs | jq -r '.[] | select(.focused) | .name') $scrnPath
-        ;;
-esac
+mv $tmpPath $scrnPath
 
 notify-send.sh $scrnPath -i $scrnPath --default-action="urxvt -e ranger --selectfile=${scrnPath}"
 #selectfile is a legacy option
