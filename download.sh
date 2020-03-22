89da75e0ff8c7a7e9ab6aa7e55f99619f66e5d94
Stivvo <entattis15@itisvinci.com>
2020-03-22 14:43:22 +0100

    new download script
    - configs in home directory are symlinks to the repo
    - configs in the repo are in their own directory

--- download.sh
+++ download.sh
@@ -1,116 +1,49 @@
 #!/bin/bash
 
-echo $(whoami)
-# update dotfiles
-mkdir ~/.vim
-cp vim/vimrc ~/.vim/
-cp vim/*.vim ~/.vim/
-cp -r vim/ftplugin ~/.vim/
-echo "vim..."
-cp snclirc ~/.snclirc
-echo "snclirc..."
-cp bashrc ~/.bashrc
-source /home/stefano/.bashrc
-echo "bashrc..."
-
-# fish
+echo "shells..."
+ln -s bashrc ~/.bashrc
+source ~/.bashrc
 #sudo chsh -s $(whereis fish | awk '{print $2}') $(whoami)
 #echo "setting default shell to fish, reboot required"
-cp fish ~/.config/fish/config.fish
-echo "fish..."
+ln -s fish ~/.config/fish
+ln -s Xresources ~/.Xresources
+xrdb ~/.Xresources
 
-# git config
+echo "creating symlinks for programs configs..."
+ln -s mako ~/.config/mako
+ln -s pam ~/.pam_environment  # pam environment, enable wayland on firefox
+ln -s vim ~/.vim/
+ln -s zathura ~/.config/zathura
+# sway
+ln -s sway ~/.config/sway
+ln -s swaylock ~/.config/swaylock
+ln -s ranger ~/.config/ranger
+
+echo "git config..."
 git config --global user.name Stivvo
 git config --global user.email entattis15@itisvinci.com
 git config --global core.editor vim
-echo "git config..."
 
-# scripts
-sudo chmod ua+x ~/prog/dotfiles/script/mark/sh
-sudo chmod ua+x ~/prog/dotfiles/script/screen/sh
-sudo chmod ua+x ~/prog/dotfiles/script/update.sh
-sudo chmod ua+x ~/prog/dotfiles/script/background/sh
-sudo chmod ua+x ~/prog/dotfiles/script/shot/shot.sh
-sudo chmod ua+x ~/prog/dotfiles/script/shot/colorPicker.sh
-sudo chmod ua+x ~/prog/dotfiles/script/bar/sh
-sudo chmod ua+x ~/prog/dotfiles/script/bar/loop.sh
-sudo chmod ua+x ~/prog/dotfiles/script/notify/light.sh
-sudo chmod ua+x ~/prog/dotfiles/script/notify/volume.sh
-sudo chmod ua+x ~/prog/dotfiles/script/smartGapsBorderToggle.sh
-sudo chmod ua+x ~/prog/dotfiles/ranger/scope.sh
-sudo cp ~/prog/dotfiles/script/intellideScript/intellide /usr/bin/
-sudo cp ~/prog/dotfiles/script/intellideScript/intellide.desktop /usr/share/applications
-sudo chmod ua+x ~/usr/bin/intellide
-echo "chmod +x useful scripts..."
-
-# simplenote
-chmod +x ~/Sync/script/snPasswd.sh
-~/Sync/script/./snPasswd.sh
-echo "setting username and password of simplenote..."
-
-# pam environment, enable wayland on firefox
-cp ~/prog/dotfiles/.pam_environment ~/
-
-# zathura
-mkdir ~/.config/zathura
-cp zathurarc ~/.config/zathura/.
-echo "zathurarc..."
-
-# i3
-mkdir ~/.config/i3
-cp ~/prog/dotfiles/i3 ~/.config/i3/config
-echo "i3 config..."
-
-# sway
-mkdir ~/.config/sway
-cp ~/prog/dotfiles/sway ~/.config/sway/config
-echo "sway config..."
-mkdir ~/.config/swaylock
-cp ~/prog/dotfiles/swaylock ~/.config/
-echo "swaylock..."
-
-# vifm
-cp ~/prog/dotfiles/vifmrc ~/.config/vifm/vifmrc
-cp ~/prog/dotfiles/script/vifm/vifmimg ~/.config/vifm/scripts/
-cp ~/prog/dotfiles/script/vifm/vifmrun ~/.config/vifm/scripts/
-echo "vifm..."
-
-# mako
-mkdir ~/.config/mako
-cp ~/prog/dotfiles/mako ~/.config/mako/config
-
-# ranger
-mkdir ~/.config/ranger/
-ranger --copy-config=all
-cp ~/prog/dotfiles/ranger/rc.conf ~/.config/ranger/
-cp ~/prog/dotfiles/ranger/scope.sh ~/.config/ranger/
-cp ~/prog/dotfiles/ranger/rifle.conf ~/.config/ranger/
-echo "ranger..."
-sudo cp ~/prog/dotfiles/Xresources ~/.Xresources
-xrdb ~/.Xresources
-
+echo "creating useful dirs..."
 mkdir ~/.cache/markdown/
 mkdir ~/.cache/markdown/mkd
 mkdir ~/.cache/markdown/html
-
-# volume and brightness notifications files
-echo 100 > /tmp/audio-notification
-echo 100 > /tmp/brightness-notification
-
-# video group for light
-sudo usermod -a -G video $USER
-
-# create screen folder
 mkdir ~/screen
 mkdir ~/.cache/screen
 
+echo "procedures..."
+# volume and brightness notifications files
+echo 100 > /tmp/audio-notification
+echo 100 > /tmp/brightness-notification
 # install all packages
 sudo pacman -S --needed trizen
 trizen -S --needed < packages.txt
-
+# video group for light
+sudo usermod -a -G video $USER
 # install vim instant markdown
 sudo npm -g install instant-markdown-d
 
+echo "oh my fish..."
 # installing fish
 curl -L https://get.oh-my.fish | fish
-omf install l
+fish --command="omf install l"
