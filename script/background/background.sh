438c907fbc2826386a2098f1a639b801a1b3f70a
Stivvo <entattis15@itisvinci.com>
2020-04-02 21:53:05 +0200

    don't run programs each sway reload

---
+++ script/background/background.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+PA="/home/stefano/wallpapers/selected/"
+P="/home/stefano/prog/dotfiles/script/background/"
+LIST="/tmp/listBg.txt"
+
+while true
+do
+    ls $PA > $LIST
+    L=$(wc -l $LIST | awk '{print $1}')
+    R=$[RANDOM % L]
+    let R++
+    IMG="$(sed -n "${R}p" $LIST)" 
+    echo "$R $IMG" > /tmp/currentBg.txt
+    swaymsg output "*" bg "${PA}${IMG}" fit
+    sleep 30m
+done
