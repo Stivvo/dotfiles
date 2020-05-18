cf2bdb7eb534e8a502159e3c402f934173898bb1
Stivvo <stivvo01@gmail.com>
2020-05-18 12:21:38 +0200

    download.sh directly use /home/stefano/prog/dotfiles

--- download.sh
+++ download.sh
@@ -2,56 +2,54 @@
 
 echo "creating useful dirs..."
 mkdir ~/.cache ~/.cache/markdown/ ~/.cache/markdown/mkd ~/.cache/markdown/html ~/screen ~/.cache/screen ~/.config ~/.config/fish ~/.config/mako ~/.config/ranger ~/.config/nvim ~/.config/zathura ~/.config/sway ~/.config/swaylock ~/.config/imv ~/.config/alacritty
-pw="${PWD}"
-echo ${pw}
 
 echo "shells..."
-ln -sf ${pw}/bashrc ~/.bashrc
+ln -sf ${PWD}/bashrc ~/.bashrc
 source ~/.bashrc
-ln -sf ${pw}/fish/config.fish ~/.config/fish/config.fish
-ln -sf ${pw}/fish/common.fish ~/.config/fish/common.fish
+ln -sf ${PWD}/fish/config.fish ~/.config/fish/config.fish
+ln -sf ${PWD}/fish/common.fish ~/.config/fish/common.fish
 echo "setting default shell to fish, reboot required"
 sudo chsh -s $(whereis fish | awk '{print $2}') $(whoami)
 
 echo "xresources..."
-ln -sf ${pw}/Xresources ~/.Xresources
+ln -sf ${PWD}/Xresources ~/.Xresources
 xrdb ~/.Xresources
 
 echo "creating symlinks for programs configs..."
-ln -sf ${pw}/pam ~/.pam_environment  # pam environment, enable wayland on firefox
-ln -sf ${pw}/mako ~/.config/mako/config
-ln -sf ${pw}/zathurarc ~/.config/zathura/zathurarc
-ln -sf ${pw}/imv ~/.config/imv/config
-ln -sf ${pw}/alacritty.yml ~/.config/alacritty/alacritty.yml
+ln -sf ${PWD}/pam ~/.pam_environment  # pam environment, enable wayland on firefox
+ln -sf ${PWD}/mako ~/.config/mako/config
+ln -sf ${PWD}/zathurarc ~/.config/zathura/zathurarc
+ln -sf ${PWD}/imv ~/.config/imv/config
+ln -sf ${PWD}/alacritty.yml ~/.config/alacritty/alacritty.yml
 
 echo "ranger..."
-ln -sf ${pw}/ranger/rc.conf ~/.config/ranger/rc.conf
-ln -sf ${pw}/ranger/rifle.conf ~/.config/ranger/rifle.conf
-ln -sf ${pw}/ranger/scope.sh ~/.config/ranger/scope.sh
+ln -sf ${PWD}/ranger/rc.conf ~/.config/ranger/rc.conf
+ln -sf ${PWD}/ranger/rifle.conf ~/.config/ranger/rifle.conf
+ln -sf ${PWD}/ranger/scope.sh ~/.config/ranger/scope.sh
 
 echo "nvim..."
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-ln -sf ${pw}/nvim/init.vim ~/.config/nvim/init.vim
-ln -sf ${pw}/nvim/plug.vim ~/.config/nvim/plug.vim
-ln -s ${pw}/nvim/plugin ~/.config/nvim/
-ln -s ${pw}/nvim/ftplugin ~/.config/nvim/
-ln -s ${pw}/nvim/other ~/.config/nvim/
+ln -sf ${PWD}/nvim/init.vim ~/.config/nvim/init.vim
+ln -sf ${PWD}/nvim/plug.vim ~/.config/nvim/plug.vim
+ln -s ${PWD}/nvim/plugin ~/.config/nvim/
+ln -s ${PWD}/nvim/ftplugin ~/.config/nvim/
+ln -s ${PWD}/nvim/other ~/.config/nvim/
 nvim -c "PlugInstall|q|q"
 
 echo "sway..."
-ln -sf ${pw}/sway ~/.config/sway/config
-ln -sf ${pw}/swaylock ~/.config/swaylock/config
-ln -s ${pw}/waybar ~/.config/
+ln -sf ${PWD}/sway ~/.config/sway/config
+ln -sf ${PWD}/swaylock ~/.config/swaylock/config
+ln -s ${PWD}/waybar ~/.config/
 
 echo "pacman config..."
-sudo chown root ${pw}/pacman/mirrorlist
-sudo chown root ${pw}/pacman/makepkg.conf
-sudo chown root ${pw}/pacman/pacman.conf
-sudo ln -sf ${pw}/pacman/mirrorlist /etc/pacman.d/mirrorlist
-sudo ln -sf ${pw}/pacman/makepkg.conf /etc/makepkg.conf
-sudo ln -sf ${pw}/pacman/pacman.conf /etc/pacman.conf
-sudo ln -sf ${pw}/pacman/mirrorlist-rankmirrors.conf /etc/pacman.d/mirrorlist-rankmirrors.conf
+sudo chown root ${PWD}/pacman/mirrorlist
+sudo chown root ${PWD}/pacman/makepkg.conf
+sudo chown root ${PWD}/pacman/pacman.conf
+sudo ln -sf ${PWD}/pacman/mirrorlist /etc/pacman.d/mirrorlist
+sudo ln -sf ${PWD}/pacman/makepkg.conf /etc/makepkg.conf
+sudo ln -sf ${PWD}/pacman/pacman.conf /etc/pacman.conf
+sudo ln -sf ${PWD}/pacman/mirrorlist-rankmirrors.conf /etc/pacman.d/mirrorlist-rankmirrors.conf
 
 echo "git config..."
 git config --global user.name Stivvo
@@ -84,7 +82,7 @@ fi
 if [ ! -d ~/.config/omf ] || [ ! -d ~/.local/share/omf ]
 then
     echo "oh my fish..."
-    curl -L https://get.oh-my.fish > ~/install.fish \
+    curl -L https://get.oh-my.fish > ~/install.fish
     fish ~/install.fish --path=~/.local/share/omf --config=~/.config/omf \
     fish --command="omf install l"
 fi
