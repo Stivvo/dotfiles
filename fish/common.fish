e1e0bc96e85ee726faa3511dddabe11392adf350
Stivvo <stivvo01@gmail.com>
2020-08-10 12:05:40 +0200

    fish detect WM to change wayland options
    Using the same configs on both sway and lxde, is important to set
    wayland options (if QT_QPA_PLATFORM is wrong, qt app don't start) only
    it sway window manager is detected
    
    Lxde: avoid suspension and set keyboard layout

--- fish/common.fish
+++ fish/common.fish
@@ -1,10 +1,7 @@
 export EDITOR=nvim
 export BROWSER=firefox-developer-edition
-export RANGER_LOAD_DEFAULT_RC=FALSE
-export MOZ_ENABLE_WAYLAND=1
-export QT_QPA_PLATFORM=wayland
+export LD_LIBRARY_PATH="/lib:/usr/lib:/usr/bin/lib:/usr/local/lib"
 
-export XDG_CURRENT_DESKTOP=sway
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_CACHE_HOME="$HOME/.cache"
 export XDG_DATA_HOME="$HOME/.local/share"
