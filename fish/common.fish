580c64e7d8e1b0f019716620a25eb8e7c3f79e84
Stivvo <stivvo01@gmail.com>
2020-08-25 14:16:26 +0200

    export vars in pam is more efficient than fish
    Now fish has a much more responsive startup
    
    Also pam is shell independent so it will always work

--- fish/common.fish
+++ fish/common.fish
@@ -1,11 +1,3 @@
-export EDITOR=nvim
-export BROWSER=firefox-developer-edition
-export LD_LIBRARY_PATH="/lib:/usr/lib:/usr/bin/lib:/usr/local/lib"
-
-export XDG_CONFIG_HOME="$HOME/.config"
-export XDG_CACHE_HOME="$HOME/.cache"
-export XDG_DATA_HOME="$HOME/.local/share"
-
 alias l='lsd -h'
 alias ll='lsd -hl'
 alias la='lsd -ha'
@@ -57,28 +49,3 @@ alias grestore='git restore'
 alias xamt='sudo xampp start'
 alias xamr='sudo xampp start'
 alias xamp='sudo xampp stop'
-
-export FFF_FAV1=$HOME/prog
-export FFF_FAV2=$HOME/wallpapers
-export FFF_FAV3=$HOME/screen
-export FFF_FAV4=$HOME/Downloads
-export FFF_FAV5=$HOME/prog/dotfiles
-export FFF_FAV6=$HOME/prog/preview/wayPreview
-export FFF_FAV7=$HOME/prog/converter/qTsConverter
-export FFF_FAV8=$HOME/.local/path
-export FFF_FAV9=/opt/lampp/htdocs/stuff
-
-export FFF_FILE_FORMAT="%f"
-export FFF_MARK_FORMAT="> %f*"
-export FFF_OPENER="xdg-open"
-export FFF_CD_ON_EXIT=1
-
-export FFF_COL1=2 # directory
-export FFF_COL2=5 # status background
-export FFF_COL3=5 # selection
-export FFF_COL4=6 # cursor
-export FFF_COL5=0 # foerground
-export FFF_LS_COLORS=0
-
-export MANGOHUD_DLSYM=1
-export MANGOHUD=1
--- fish/config.fish
+++ fish/config.fish
@@ -3,19 +3,6 @@ set fish_greeting
 
 source $HOME/.config/fish/common.fish
 
-if test $DESKTOP_SESSION = "LXDE"
-    setxkbmap it
-    xset s off
-    xset -dpms
-    export XDG_CURRENT_DESKTOP=lxde
-    export MOZ_ENABLE_WAYLAND=0
-    export QT_QPA_PLATFORM=xcb
-else if test $DESKTOP_SESSION = "/usr/share/wayland-sessions/sway"
-    export XDG_CURRENT_DESKTOP=sway
-    export MOZ_ENABLE_WAYLAND=1
-    export QT_QPA_PLATFORM=wayland
-end
-
 function f
     fff $argv
     cd (cat $XDG_CACHE_HOME/fff/.fff_d)
