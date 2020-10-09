55ec3d5701bd58e7cb86738f431834530b1e2675
Stivvo <stivvo01@gmail.com>
2020-10-09 23:13:14 +0200

    editor.sh parameter support

--- script/init/editor.sh
+++ script/init/editor.sh
@@ -1,3 +1,3 @@
 #!/bin/dash
 
-nvim --cmd "source $XDG_CONFIG_HOME/nvim/other/vim-plug.vim" --cmd "source $XDG_CONFIG_HOME/nvim/other/gui.vim"
+nvim --cmd "source $XDG_CONFIG_HOME/nvim/other/vim-plug.vim" --cmd "source $XDG_CONFIG_HOME/nvim/other/gui.vim" $@
