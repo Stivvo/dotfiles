c1c422724adff9e1d6807bffc1379af7fb307bb6
Stivvo <stivvo01@gmail.com>
2020-11-03 18:49:39 +0100

    Start programs without additional scripts
    udiskie.sh and update.sh were used only by dwl.sh, so it makes sense to
    put them directly in there to reduce overhead

--- script/init/dwl.sh
+++ script/init/dwl.sh
@@ -5,8 +5,12 @@ corectrl &
 waybar &
 monitor.sh r
 background.sh &
+
 redshift -l 45.47:9.19 -t 6500:4500 -m wayland &
-udiskie.sh &
-update.sh &
+udiskie --appindicator --tray --automount --notify --file-manager "alacritty --title 'FFF disk' -e fff " &
+/usr/bin/pikaur -Syuw --noconfirm &&
+    /usr/bin/notify-send.sh -r 10000 "Updated pacman packages" ||
+    /usr/bin/notify-send.sh -r 10000 "ERROR updating packages" &
+
 kdeconnect-indicator &
 normal.mice.sh
--- script/init/udiskie.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/dash
-
-udiskie --appindicator --tray --automount --notify --file-manager "alacritty --title 'FFF disk' -e fff "
