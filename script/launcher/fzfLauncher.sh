a638285737d3cf6206941b286be864884b937a35
Stivvo <stivvo01@gmail.com>
2020-11-13 16:22:42 +0100

    Execute *.menu.sh in another alacritty wrapper
    Executables which end in .menu.sh are executed in another floating
    alacritty window, the same as fzfLauncher.sh

--- script/launcher/fzfLauncher.sh
+++ script/launcher/fzfLauncher.sh
@@ -1,3 +1,7 @@
 #!/bin/dash
 
-exec setsid $(cat $XDG_CACHE_HOME/pathCache | fzf)
+EXT=".menu.sh"
+CMD=$(cat $XDG_CACHE_HOME/pathCache | fzf)
+STRIP=${CMD%%$EXT}
+[ "$CMD" != "$STRIP" ] && CMD="alacritty --class fzfLauncher.sh -t $CMD -e $CMD"
+exec setsid $CMD
