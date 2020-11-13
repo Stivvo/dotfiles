d917713c8cee2baf55a43c98219fd78c9fbccb60
Stivvo <stivvo01@gmail.com>
2020-11-13 17:23:06 +0100

    Keep .menu.sh scripts in the same alacritty window

--- script/launcher/fzfLauncher.sh
+++ script/launcher/fzfLauncher.sh
@@ -3,5 +3,5 @@
 EXT=".menu.sh"
 CMD=$(cat $XDG_CACHE_HOME/pathCache | fzf)
 STRIP=${CMD%%$EXT}
-[ "$CMD" != "$STRIP" ] && CMD="alacritty --class fzfLauncher.sh -t $CMD -e $CMD"
-exec setsid $CMD
+[ $CMD = $STRIP ] && CMD="exec setsid $CMD"
+$CMD
