340cbed8325c5abe54cdd291d0293d69162d7142
Stivvo <stivvo01@gmail.com>
2020-11-17 16:58:03 +0100

    Fix mako and redshift when changing monitors
    Mount remote videos on dwl startup
    
    redshift.sh restarts mako and redshift. It can easily be executed after
    a monitor change if needed
    
    dwl.sh is now faster

--- script/init/dwl.sh
+++ script/init/dwl.sh
@@ -1,14 +1,14 @@
 #!/bin/dash
 
-mako &
 corectrl &
 waybar &
+redshift.sh &
 monitor.sh r
 background.sh &
 udiskie --appindicator --tray --automount --notify --file-manager "alacritty --title 'FFF disk' -e fff " &
 kdeconnect-indicator &
-redshift.sh &
 normal.mice.sh
+sshfs media@olidata.stivvo01.com:/home/media/video ${XDG_VIDEOS_DIR}/remote/
 pikaur -Syuw --noconfirm &&
     notify-send.sh -r 10000 "Updated pacman packages" ||
     notify-send.sh -r 10000 "ERROR updating packages"
