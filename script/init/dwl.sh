44816ccfff016c8ab9ea4d8ae0cdb55d9c66974b
Stivvo <stivvo01@gmail.com>
2020-11-21 21:30:15 +0100

    Start undervolt with dwl

--- script/init/dwl.sh
+++ script/init/dwl.sh
@@ -8,6 +8,7 @@ background.sh &
 udiskie --appindicator --tray --automount --notify --file-manager "alacritty --title 'FFF disk' -e fff " &
 kdeconnect-indicator &
 normal.mice.sh
+sudo undervolt --gpu -75 --core -100 --cache -100 --uncore -100 --analogio -100
 sshfs media@olidata.stivvo01.com:/home/media/video ${XDG_VIDEOS_DIR}/remote/
 pikaur -Syuw --noconfirm &&
     notify-send.sh -r 10000 "Updated pacman packages" ||
