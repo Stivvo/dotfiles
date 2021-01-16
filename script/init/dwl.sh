594ba3c9a4dcd4b5ad6eb2dbb75324435632df29
Stivvo <stivvo01@gmail.com>
2021-01-16 11:24:10 +0100

    Don't start external programs in dwl.sh
    Leave persona / external programs to personal.sh (not included in the
    repo)

--- script/init/dwl.sh
+++ script/init/dwl.sh
@@ -1,10 +1,7 @@
 #!/bin/dash
 
-corectrl &
 waybar &
 redshift.sh
 monitor.sh r
 background.sh &
-udiskie --appindicator --tray --automount --notify --file-manager "alacritty --title 'FFF disk' -e fff " &
-kdeconnect-indicator &
 personal.sh
