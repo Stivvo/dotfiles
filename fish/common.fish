f617a73a523863e5b1377cbf1e7fbb2864891072
Stivvo <stivvo01@gmail.com>
2021-01-30 19:00:01 +0100

    Remove unused git, grep, xampp aliases
    Shorten some git aliases

--- fish/common.fish
+++ fish/common.fish
@@ -16,30 +16,21 @@ alias q='xbps-query -Rs'
 alias i='sudo xbps-install -S'
 alias u='sudo xbps-install -Su'
 
-alias grep="grep --color"
 alias fgrep="fgrep --color"
 alias egrep="egrep --color"
-alias grepi="grep --color -r -i"
 alias fgrepi="fgrep --color -r -i"
 alias egrepi="egrep --color -r -i"
 
 alias clone='git clone --recursive'
-alias pull='git pull'
-alias commit='git commit'
-alias commim='git commit -m'
+alias pul='git pull'
+alias com='git commit'
 alias push='git push'
-alias pusf='git push -f'
 alias ad='git add'
 alias ada='git add -A'
-alias branca='git branch --all'
-alias remotv='git remote -v'
-alias rebasi='git rebase -i'
+alias bra='git branch --all'
+alias remv='git remote -v'
 alias gs='git switch'
 alias glog='git log --color=always'
 alias glol='git log --pretty=oneline --color=always'
 alias gdiff='git diff --color'
 alias gt='git status'
-
-alias xamt='sudo xampp start'
-alias xamr='sudo xampp start'
-alias xamp='sudo xampp stop'
