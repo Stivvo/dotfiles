f117c17fd9d0e09f8a8cf7fc86cd25c9588345a3
Stivvo <stivvo01@gmail.com>
2021-06-24 18:25:44 +0200

    Link hosts.(new|old) to /etc/hosts instead of copy

--- script/init/dwlstart.sh
+++ script/init/dwlstart.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-doas cp /etc/hosts.new /etc/hosts
+doas ln -sf /etc/hosts.new /etc/hosts
 $HOME/.local/script/variables.sh 1 /usr/local/bin/dwl -s $HOME/.local/script/dwl.sh
-doas cp /etc/hosts.base /etc/hosts
+doas ln -sf /etc/hosts.base /etc/hosts
